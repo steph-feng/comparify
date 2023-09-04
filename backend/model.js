7198db63fc506553d9bfedac6310df387accd44d
Stephanie Feng <sfeng19@students.cs.ubc.ca>
2023-09-03 18:25:09 -0700

    First draft

--- backend/model.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
-
-const userSchema = new Schema({
-    topArtists: [{ name: String }],
-    topTracks: [{ name: String }]
-}
-)
-module.exports = mongoose.model('User', userSchema);
